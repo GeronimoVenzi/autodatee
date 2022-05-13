94bef07f0004dce7e9c3ab30c8de23ac72d2dc5d
geronimovenzi <geronimovenzi@live.com.ar>
2022-05-13 05:56:46 -0300

    agrego simulador de chat

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 const $form = document.querySelector('#contact-form')
 const $buttonMailTo = document.querySelector('#mail-to')
+const chat = document.getElementById('chat')
+const burbuja = document.getElementById('burbuja')
+
+
+burbuja.addEventListener('click', () => {
+    chat.classList.toggle('open')
+})
 
 $form.addEventListener('submit', handleSubmit)
 
