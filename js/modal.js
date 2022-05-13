cebf25d8fb305c2f6fd908dd7417588c90245c62
geronimovenzi <geronimovenzi@live.com.ar>
2022-05-13 15:30:48 -0300

    termino de arreglar el chat para whatsapp

--- js/modal.js
+++
@@ -1,6 +0,0 @@
-const chat = document.getElementById('chat');
-const burbuja = document.getElementById('burbuja')
-
-burbuja.addEventListener('click', () => {
-    chat.classList.toggle('open');
-})
